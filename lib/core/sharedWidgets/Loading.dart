e4ffec737176caef7480a026e4cc51f356f1de80
MohammadRakib <mohammadrakibzaman@gmail.com>
2022-08-06 21:41:21 +0600

    some changes

--- lib/core/sharedWidgets/Loading.dart
+++ lib/core/sharedWidgets/Loading.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:quizhouse/core/utils/color.dart';
 class Loading extends StatelessWidget {
   const Loading({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.brown.shade50,
+      color: Colors.purple.shade50,
       child: const Center(
         child: SpinKitWanderingCubes(
-          color: Colors.brown,
+          color: Color(primaryColor),
           size: 50.0,
         ),
       ),
